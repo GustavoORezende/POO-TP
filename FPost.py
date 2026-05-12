25c9b41ebc6c69a880a9948f8f9ad39e6710f5cc
Gustavo Rezende <gustavo.rezende@elogroup.com.br>
2026-05-12 20:29:23 -0300

    Encapsulating attributes

--- FPost.py
+++ FPost.py
@@ -1,29 +1,47 @@
 import json 
 import os
 from datetime import datetime
-
+#TODO: COMENTARIOS NO CODIGO
 class User:
     def __init__(self, nome, sobrenome):
-        self.nome = nome
-        self.sobrenome = sobrenome
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+
+    def getNome(self):
+        return self.__nome
+    
+    def getSobrenome(self):
+        return self.__sobrenome
        
 
 class Post:
     def __init__(self, user, texto):
         self.autor = {
-            "nome": user.nome,
-            "sobrenome": user.sobrenome
+            "nome": user.getNome(),
+            "sobrenome": user.getSobrenome()
         }
         self.texto = texto
-        self.curtidas = 0
-        self.comentarios = []
-        self.data = datetime.now().strftime("%d/%m/%Y %H:%M")
+        self.__curtidas = 0
+        self.__comentarios = []
+        self.__data = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    def toDict(self):
+        return{
+            "autor": self.autor,
+            "texto": self.texto,
+            "curtidas": self.__curtidas,
+            "comentarios": self.__comentarios,
+            "data": self.__data
+
+        }
         
     def curtir(self):
-        self.curtidas =+ 1
+        self.__curtidas += 1
 
     def comentar(self, comentario):
-        self.comentarios.append(comentario)
+        if comentario.strip()=="":
+            raise ValueError("Comentario nao pode ser vazio")
+        self.__comentarios.append(comentario)
     
 
     def exibirAutor(self):
@@ -34,22 +52,22 @@ class Post:
        
 class Feed:
     def __init__(self, arquivo):
-        self.arquivo = arquivo
+        self.__arquivo = arquivo
 
     def postar(self, post):
         try:
             try:
-                with open(self.arquivo, 'r', encoding='utf-8') as arquivo:
+                with open(self.__arquivo, 'r', encoding='utf-8') as arquivo:
                     posts = json.load(arquivo)
             
             except:
                 posts =[]
 
-            posts.append(post.__dict__)
+            posts.append(post.toDict())
 
-            with open(self.arquivo, 'w', encoding='utf-8') as arquivo:
+            with open(self.__arquivo, 'w', encoding='utf-8') as arquivo:
                 json.dump(posts, arquivo, indent=4, ensure_ascii=False)
-                print(f"Sucesso: sua publicacao foi postada em {self.arquivo}")
+                print(f"Sucesso: sua publicacao foi postada em {self.__arquivo}")
 
         except Exception as e:
             print(f"Erro ao postar: {e}")
